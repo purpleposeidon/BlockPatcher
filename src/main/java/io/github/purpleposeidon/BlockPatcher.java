a66b5fb3e59d103018c67e702ab1a95e30c3011b
neptunepink <purpleposeidon@gmail.com>
2016-04-11 17:58:48 -0700

    Modify the chunk NBT data w/o standard loading mechanisms.

--- src/main/java/io/github/purpleposeidon/BlockPatcher.java
+++ src/main/java/io/github/purpleposeidon/BlockPatcher.java
@@ -16,10 +16,13 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.StringUtils;
 import net.minecraft.world.MinecraftException;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.NibbleArray;
 import net.minecraft.world.chunk.storage.AnvilChunkLoader;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraft.world.chunk.storage.IChunkLoader;
 import net.minecraft.world.chunk.storage.RegionFile;
 import net.minecraft.world.storage.ISaveHandler;
@@ -27,11 +30,12 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
-import net.minecraftforge.event.world.ChunkEvent;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.event.world.WorldEvent;
 import org.apache.logging.log4j.Logger;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
@@ -45,11 +49,11 @@ public class BlockPatcher {
     Logger log;
 
     static final int MAX_BLOCK_ID = 4095;
-    IPatch[] transforms = new IPatch[MAX_BLOCK_ID];
+    int[] transforms = new int[MAX_BLOCK_ID];
 
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
-        for (int i = 0; i < transforms.length; i++) transforms[i] = IPatch.NULL;
+        for (int i = 0; i < transforms.length; i++) transforms[i] = i;
 
         MinecraftForge.EVENT_BUS.register(this);
         FMLCommonHandler.instance().bus().register(this);
@@ -58,50 +62,18 @@ public class BlockPatcher {
         config = new Configuration(configFile);
         block_patches = config.getCategory("blockPatches");
         item_patches = config.getCategory("itemPatches");
-        block_patches.setComment("Format:\nI:id=minecraft:blockname#optionalMetadata");
         config.save();
         for (Map.Entry<String, Property> entry : block_patches.entrySet()) {
             String idS = entry.getKey();
             String repl = entry.getValue().getString();
             int id = Integer.parseInt(idS);
-            transforms[id] = parsePatch(repl);
+            transforms[id] = parsePatch(id, repl);
         }
     }
 
-    private IPatch parsePatch(String repl) {
-        if (StringUtils.isNullOrEmpty(repl)) return IPatch.NULL;
-        String parts[] = repl.split("#");
-        String id = parts[0];
-        int md = -1;
-        if (parts.length > 1) {
-            md = Integer.parseInt(parts[1]);
-        }
-        Block b = GameData.getBlockRegistry().getObject(id);
-        return new ReplaceWith(b, md);
-    }
-
-    static class ReplaceWith implements IPatch {
-        final Block to;
-        final int md;
-
-        public ReplaceWith(Block to, int md) {
-            this.to = to;
-            this.md = md;
-        }
-
-        public ReplaceWith(Block to) {
-            this.to = to;
-            this.md = -1;
-        }
-
-        @Override
-        public void apply(Chunk chunk, int cx, int y, int cz) {
-            int md = this.md;
-            if (md == -1) {
-                md = chunk.getBlockMetadata(cx, y, cz);
-            }
-            chunk.func_150807_a(cx, y, cz, to, md);
-        }
+    private int parsePatch(int defaultId, String repl) {
+        if (StringUtils.isNullOrEmpty(repl)) return defaultId;
+        return GameData.getBlockRegistry().getId(repl);
     }
 
     @Mod.EventHandler
@@ -130,7 +102,7 @@ public class BlockPatcher {
     }
 
     Joiner j = Joiner.on(" ");
-    void log(String... msg) {
+    void log(Object... msg) {
         log.warn(j.join(msg));
     }
 
@@ -161,48 +133,86 @@ public class BlockPatcher {
         f = new File(f, "region");
         File[] files = f.listFiles();
         if (files == null) return;
+        final FMLControlledNamespacedRegistry<Block> blockRegistry = GameData.getBlockRegistry();
+        int regionFiles = 0;
         for (File regionName : files) {
             if (!regionName.toString().endsWith(".mca")) continue;
-            log("Converting ", regionName.toString());
+            regionFiles++;
+        }
+        int regionFileIndex = 0;
+        for (File regionName : files) {
+            if (!regionName.toString().endsWith(".mca")) continue;
+            regionFileIndex++;
             RegionFile rf = new RegionFile(regionName);
+            int changes = 0;
+            int chunkCount = 0;
             for (int rx = 0; rx < 32; rx++) {
                 for (int rz = 0; rz < 32; rz++) {
+                    // A region is a sparse 32x32 of chunks. See RegionFile.outOfBounds()
                     if (!rf.chunkExists(rx, rz)) continue;
+                    chunkCount++;
                     DataInputStream dis = rf.getChunkDataInputStream(rx, rz);
                     NBTTagCompound tag = CompressedStreamTools.read(dis);
                     dis.close();
                     if (tag == null) continue;
                     NBTTagCompound Level = tag.getCompoundTag("Level");
                     if (Level == null) continue;
-                    int x = Level.getInteger("xPos");
-                    int z = Level.getInteger("zPos");
-                    Chunk chunk = loader.loadChunk(event.world, x, z);
-                    if (chunk == null) continue;
-                    MinecraftForge.EVENT_BUS.post(new ChunkEvent.Load(chunk));
-                    processChunk(chunk);
-                    if (!chunk.isModified) continue;
-                    loader.saveChunk(event.world, chunk);
-                    loader.saveExtraChunkData(event.world, chunk);
+                    int chunkX = Level.getInteger("xPos");
+                    int chunkZ = Level.getInteger("zPos");
+                    NBTTagList Sections = Level.getTagList("Sections", Constants.NBT.TAG_COMPOUND);
+                    if (Sections == null) continue;
+                    boolean changed = false;
+                    // We simply have to operate on raw NBT data. There's far too much modded/vanilla squirreliness
+                    // going on to load the chunk through normal mechanisms.
+                    // See AnvilChunkLoader.readChunkFromNBT and ExtendedBlockStorage
+                    for (int sId = 0; sId < Sections.tagCount(); sId++) {
+                        NBTTagCompound Section = Sections.getCompoundTagAt(sId);
+                        byte Y = Section.getByte("Y");
+                        ExtendedBlockStorage ebs = new ExtendedBlockStorage(Y << 4, false /* hasSky. We don't need it. */);
+                        byte[] LSB = Section.getByteArray("Blocks");
+                        ebs.setBlockLSBArray(LSB);
+                        NibbleArray MSBA = null;
+
+                        if (Section.hasKey("Add", Constants.NBT.TAG_BYTE_ARRAY)) {
+                            ebs.setBlockMSBArray(MSBA = new NibbleArray(Section.getByteArray("Add"), 4));
+                        }
+
+                        ebs.setBlockMetadataArray(new NibbleArray(Section.getByteArray("Data"), 4));
+
+                        for (int slabY = 0; slabY < 0x10; slabY++) {
+                            for (int slabZ = 0; slabZ < 0x10; slabZ++) {
+                                for (int slabX = 0; slabX < 0x10; slabX++) {
+                                    // This iteration order has us stepping through the LSB efficiently.
+                                    int i = slabY << 8 | slabZ << 4 | slabX;
+                                    int oldId = LSB[i] & 255;
+                                    if (MSBA != null) {
+                                        oldId |= MSBA.get(slabX, slabY, slabZ) << 8;
+                                    }
+                                    int replacement = transforms[oldId];
+                                    if (replacement == oldId) continue;
+                                    ebs.func_150818_a/* setBlock */(slabX, slabY, slabZ, blockRegistry.getObjectById(replacement));
+                                    MSBA = ebs.getBlockMSBArray();
+                                    changed = true;
+                                }
+                            }
+                        }
+
+                        if (changed && MSBA != null && !Section.hasKey("Add")) {
+                            Section.setByteArray("Add", MSBA.data);
+                        }
+                    }
+
+                    if (!changed) continue;
+                    changes++;
+                    // NOTE: Relies on the data arrays being impure
+                    DataOutputStream dos = rf.getChunkDataOutputStream(rx, rz);
+                    CompressedStreamTools.write(tag, dos);
+                    dos.close();
                 }
             }
-
+            String perc = "[" + (regionFileIndex * 100 / regionFiles) + "%]";
+            log(perc + regionName.toString() + ": " + changes + " / " + chunkCount + " chunks modified");
         }
-    }
-
-    private void processChunk(Chunk chunk) {
-        for (int cx = 0; cx < 0x10; cx++) {
-            for (int cz = 0; cz < 0x10; cz++) {
-                for (int y = 0; y < 0x100; y++) {
-                    processLocation(chunk, cx, y, cz);
-                }
-            }
-        }
-    }
-
-    private void processLocation(Chunk chunk, int cx, int y, int cz) {
-        Block b = chunk.getBlock(cx, y, cz);
-        int id = GameData.getBlockRegistry().getId(b);
-        transforms[id].apply(chunk, cx, y, cz);
     }
 
 }
